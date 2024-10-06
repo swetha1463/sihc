c75d4b5326f69365a7d7e69a915f16982e3fd3b6
swetha1463 <2216163@saec.ac.in>
2024-10-06 20:58:42 +0530

    Create domain_intelligence.js

---
+++ next-app/pages/domain_intelligence.js
@@ -0,0 +1,16 @@
+// next-app/pages/domain_intelligence.js
+import Head from 'next/head';
+
+function DomainIntelligence() {
+  return (
+    <div>
+      <Head>
+        <title>Domain Intelligence</title>
+      </Head>
+      <h1>Domain Intelligence</h1>
+      <p>This is the domain intelligence page.</p>
+    </div>
+  );
+}
+
+export default DomainIntelligence;
