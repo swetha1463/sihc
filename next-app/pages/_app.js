5bcb58f0762e1bda2249e4c1c858b75d29a52d93
swetha1463 <2216163@saec.ac.in>
2024-10-06 22:10:08 +0530

    Update _app.js

--- next-app/pages/_app.js
+++ next-app/pages/_app.js
@@ -1,34 +1,9 @@
-// next-app/pages/_app.js
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { getDomainIntelligence, detectAdvancedThreats, mapCybercrimeNetworks } from '../realtime_domain_intelligence';
 
 function App() {
   const [data, setData] = useState([]);
   const [prediction, setPrediction] = useState(null);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const jsonData = Object.fromEntries(formData.entries());
-    const response = await axios.post('http://localhost:5000/cybersecurity', jsonData);
-    setPrediction(response.data.prediction);
-  };
-
-  return (
-    <div>
-      <Head>
-        <title>Cybersecurity</title>
-      </Head>
-      <h1>Cybersecurity</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Data:</label>
-        <input type="text" name="data" />
-        <button type="submit">Submit</button>
-      </form>
-      {prediction && <p>Prediction: {prediction}</p>}
-    </div>
-  );
-}
-
-export default App;
+ 
