0119884a961b3d8cae733b85c5b2064469f2ecfd
swetha1463 <2216163@saec.ac.in>
2024-10-06 20:59:15 +0530

    Create threat_detection.js

---
+++ next-app/pages/threat_detection.js
@@ -0,0 +1,16 @@
+// next-app/pages/threat_detection.js
+import Head from 'next/head';
+
+function ThreatDetection() {
+  return (
+    <div>
+      <Head>
+        <title>Threat Detection</title>
+      </Head>
+      <h1>Threat Detection</h1>
+      <p>This is the threat detection page.</p>
+    </div>
+  );
+}
+
+export default ThreatDetection;
